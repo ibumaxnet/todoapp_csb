407a7faed7137289ec5738a74fe785b131a8fd27
ibumaxnet <50646522+ibumaxnet@users.noreply.github.com>
2021-01-22 15:52:55 +0900

    javascript create javascriptで作りました

--- src/index.js
+++ src/index.js
@@ -5,57 +5,70 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  // 入力内容を元に未完了のToDoを追加する
-  const incompDiv = document.createElement("div");
-  // div作成
-  incompDiv.className = "list-row";
-
-  // li作成
-  const incompLi = document.createElement("li");
-  incompLi.innerText = inputText;
-
-  // button 作成
-  const compBtn = document.createElement("button");
-  compBtn.innerText = "完了";
-  compBtn.addEventListener("click", () => {
-    const relocateTarget = compBtn.parentNode;
-    // ボタンの前の要素のtextを取得
-    const relocateText = compBtn.previousElementSibling.innerText;
-    document.getElementById("incomplete-list").removeChild(relocateTarget);
-
-    // 完了に追加
-    const compDiv = document.createElement("div");
-    compDiv.className = "complete-list";
-    const compLi = document.createElement("li");
-    compLi.innerText = relocateText;
-    const incompBtn = document.createElement("button");
-    incompBtn.innerText = "戻す";
-
-    // 未完了リストに追加
-    compDiv.appendChild(compLi);
-    compLi.appendChild(incompBtn);
-    document.getElementById("complete-list").appendChild(compDiv);
-    console.log(compDiv);
-  });
-
-  const deleBtn = document.createElement("button");
-  deleBtn.innerText = "削除";
-  deleBtn.addEventListener("click", () => {
-    // 削除ボタンで削除
-    const delTarget = deleBtn.parentNode;
-    document.getElementById("incomplete-list").removeChild(delTarget);
-  });
-
-  // div に li を追加
-  incompDiv.appendChild(incompLi);
-  incompDiv.appendChild(compBtn);
-  incompDiv.appendChild(deleBtn);
-
-  // 未完了リストに追加
-  document.getElementById("incomplete-list").appendChild(incompDiv);
-  // console.log(deleBtn);
+  incompListAdd(inputText);
 };
 
 document
   .getElementById("add-btn")
   .addEventListener("click", () => onClickAdd());
+
+// 削除ボタンで削除
+function delListAct(target, listname) {
+  console.log("削除", target);
+  document.getElementById(listname).removeChild(target);
+}
+
+// 未完Listを作成
+function incompListAdd(listText) {
+  const incompCntDiv = document.createElement("div");
+  incompCntDiv.className = "list-row";
+
+  const incompCntLi = document.createElement("li");
+  incompCntLi.innerText = listText;
+
+  const incompCompBtn = document.createElement("button");
+  incompCompBtn.innerText = "完了";
+  incompCompBtn.addEventListener("click", () => {
+    const delTarget = incompCompBtn.parentNode;
+    delListAct(delTarget, "incomplete-list");
+    compListAdd(listText);
+  });
+
+  const incompDelBtn = document.createElement("button");
+  incompDelBtn.innerText = "削除";
+  incompDelBtn.addEventListener("click", () => {
+    const delTarget = incompCompBtn.parentNode;
+    delListAct(delTarget, "incomplete-list");
+    console.log("削除", listText);
+  });
+
+  incompCntDiv.appendChild(incompCntLi);
+  incompCntDiv.appendChild(incompCompBtn);
+  incompCntDiv.appendChild(incompDelBtn);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(incompCntDiv);
+}
+
+// 完了に追加
+function compListAdd(contText) {
+  const compConDiv = document.createElement("div");
+  compConDiv.className = "list-row";
+  const compConLi = document.createElement("li");
+  compConLi.innerText = contText;
+  const compConIncompBtn = document.createElement("button");
+  compConIncompBtn.innerText = "戻す";
+  compConIncompBtn.addEventListener("click", () => {
+    const restoreTarget = compConIncompBtn.parentNode;
+    // ボタンの前の要素のtextを取得
+    const restoreText = compConIncompBtn.previousElementSibling.innerText;
+    delListAct(restoreTarget, "complete-list");
+
+    incompListAdd(restoreText);
+  });
+
+  // 完了リストに追加
+  compConDiv.appendChild(compConLi);
+  compConDiv.appendChild(compConIncompBtn);
+  document.getElementById("complete-list").appendChild(compConDiv);
+}
